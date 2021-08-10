ea05c25f33e8a3074d6b770abe64c7af7971a5ce
Himanshu kumar <51115513+himanshukumar9@users.noreply.github.com>
2021-08-10 18:33:12 +0530

    Update dbConnection.php

--- dbConnection.php
+++ dbConnection.php
@@ -1,5 +1,5 @@
 <?php
 //all the variables defined here are accessible in all the files that include this one
-$con= new mysqli('localhost','root','','project')or die("Could not connect to mysql".mysqli_error($con));
+$con= new mysqli('sql209.epizy.com','epiz_29382543','0EnmLfwwMG27vsG','epiz_29382543_onlineexamination')or die("Could not connect to mysql".mysqli_error($con));
 
 ?>
